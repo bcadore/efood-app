0b717f0c9a865f2f342591ff6bdda831e5287510
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-11-25 16:37:15 -0300

    Alterado carrinho de compras. Adicionado lógica de contagem aos produtos no carrinho.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,35 @@
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import type { RootReducer } from '../../store'
+import { open } from '../../store/reducers/cart'
+
 import logo from "../../assets/logo.png";
 
-import { BackgroundImage, LogoImage, Phrase,  } from "./styles";
-
+import { BackgroundImage, LogoImage, Phrase } from "./styles";
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch()
+  
+  const { items } = useSelector((state: RootReducer) => state.cart)
 
   const handleLogoClick = () => {
     navigate("/");
   };
+
+  const openCart = () => {
+    dispatch(open())
+  }
 
   return (
     <BackgroundImage>
       <div className="container">
         <Phrase>Restaurantes</Phrase>
         <LogoImage src={logo} alt="Logo do EFOOD." onClick={handleLogoClick} />
-        <Phrase>0 produto(s) no carrinho</Phrase>
+        
+        <Phrase onClick={openCart} style={{ cursor: 'pointer' }}>
+            {items.length} produto(s) no carrinho
+        </Phrase>
       </div>
     </BackgroundImage>
   );
