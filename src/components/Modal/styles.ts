c279f33a7585f6708aa045bd6c4fb4e5816d0305
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-11-25 00:36:20 -0300

    Alterado estilização do modal.

---
+++ src/components/Modal/styles.ts
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import { colors } from "../..";
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+export const ModalContent = styled.div`
+  background-color: ${colors.primary};
+  padding: 32px;
+  max-width: 1024px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  gap: 24px;
+  color: ${colors.secondary};
+  
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 90%;
+    padding: 16px;
+    max-height: 90vh;
+    overflow-y: auto;
+  }
+`;
+
+export const ModalImage = styled.img`
+  width: 280px;
+  height: 280px;
+  object-fit: cover;
+  
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 200px;
+  }
+`;
+
+export const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    h3 {
+        font-size: 18px;
+        font-weight: 900;
+        margin-bottom: 16px;
+    }
+
+    p {
+        font-size: 14px;
+        line-height: 22px;
+        margin-bottom: 16px;
+    }
+`
+export const Button = styled.button`
+  background-color: ${colors.secondary};
+  color: ${colors.primary};
+  border: none;
+  font-weight: bold;
+  font-size: 14px;
+  cursor: pointer;
+  padding: 8px 16px;
+  width: fit-content;
+  margin-top: 16px;
+`;
+
+export const CloseButton = styled.img`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  cursor: pointer;
+  width: 16px;
+  height: 16px;
+  filter: brightness(0) invert(1); 
+`;
+
